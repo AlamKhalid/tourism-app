2d6c86999b9e1d600b7f90b09607582d3529aea4
Alam Khalid <alam.khalid@devsinc.com>
2022-05-18 18:56:43 +0500

    initial commit

---
+++ lib/pages/detail_pages.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:tourism_app/widgets/app_buttons.dart';
+import 'package:tourism_app/widgets/app_text.dart';
+import 'package:tourism_app/widgets/responsive_button.dart';
+import '../misc/colors.dart';
+import '../widgets/app_large_text.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  int gottenStars = 3;
+  int selectedIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.maxFinite,
+                height: 350,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/mountain.jpeg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 50,
+              left: 20,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.menu),
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 300,
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppLargeText(
+                          text: 'Yosemite',
+                          color: Colors.black.withOpacity(
+                            0.8,
+                          ),
+                        ),
+                        AppLargeText(text: '\$ 250', color: AppColors.mainColor)
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: AppColors.mainColor,
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        AppText(
+                            text: 'USA California',
+                            color: AppColors.textColor1),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(
+                            5,
+                            (index) => Icon(
+                              Icons.star,
+                              color: index < gottenStars
+                                  ? AppColors.starColor
+                                  : AppColors.textColor2,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        AppText(text: '(4.0)', color: AppColors.textColor2),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    AppLargeText(
+                      text: 'People',
+                      color: Colors.black.withOpacity(0.8),
+                      size: 20,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    AppText(
+                      text: 'Number of people in your group',
+                      color: AppColors.mainTextColor,
+                    ),
+                    const SizedBox(height: 10),
+                    Wrap(
+                      children: List.generate(
+                        5,
+                        (index) => InkWell(
+                          onTap: () {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(right: 10),
+                            child: AppButtons(
+                                color: selectedIndex == index
+                                    ? Colors.white
+                                    : Colors.black,
+                                size: 50,
+                                backgroundColor: selectedIndex == index
+                                    ? Colors.black
+                                    : AppColors.buttonBackground,
+                                borderColor: selectedIndex == index
+                                    ? Colors.black
+                                    : AppColors.buttonBackground,
+                                text: (index + 1).toString()),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    AppLargeText(
+                        text: 'Description',
+                        color: Colors.black.withOpacity(0.8),
+                        size: 22),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    AppText(
+                      text:
+                          'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been',
+                      color: AppColors.mainTextColor,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+                bottom: 20,
+                left: 20,
+                right: 20,
+                child: Row(
+                  children: [
+                    AppButtons(
+                      color: AppColors.textColor1,
+                      backgroundColor: Colors.white,
+                      borderColor: AppColors.textColor1,
+                      size: 60,
+                      isIcon: true,
+                      icon: Icons.favorite,
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    ResponsiveButton(
+                      isResponsive: true,
+                    ),
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
